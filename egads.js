734ab7e725c09f0394b2ce6f7caebbc72a5623d6
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-07-18 11:41:59 -0400

    Weird new line bug

--- egads.js
+++ egads.js
@@ -7,28 +7,28 @@ var createInstance = function(obj, parent, props) {
 	);
 	return obj;
 };
-​
+
 var processArgs = function(message, status, name, fields){
 	if(message instanceof Object) return message;
 	return { message, status, name, fields };
 };
-​
+
 var BaseError = createInstance(function(){}, Error);
 BaseError.extend = function(message, status, name, fields){
 	var args = processArgs(message, status, name, fields);
 	var subError = function(message, status, name, fields){
 		if(!(this instanceof subError)) return new subError(message, status, name, fields);
 		var args = processArgs(message, status, name, fields);
-​
+
 		this.name    = args.name    || this.name;
 		this.status  = args.status  || this.status;
 		this.message = args.message || this.message;
 		this.fields  = args.fields  || this.fields;
-​
+
 		Error.captureStackTrace(this, this.constructor);
 	};
 	subError.extend = this.extend;
-​
+
 	return createInstance(subError, this, {
 		name    : args.name    || this.prototype.name,
 		status  : args.status  || this.prototype.status,
@@ -36,4 +36,4 @@ BaseError.extend = function(message, status, name, fields){
 		fields  : args.fields  || this.prototype.fields
 	});
 };
-module.exports = BaseError.extend(null, 500, 'BaseError', {});
+module.exports = BaseError.extend('Unexpected Error.', 500, 'BaseError', {});
